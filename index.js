86b4674eb5c4ab89680e1a2463a6d3f45758cf8d
Cyril Rafalovich <replanttrees@gmail.com>
2025-05-07 09:10:24 -1000

    Five Functions added, calculateTax,convertToUpeerCase,findMaximum,isPalindromw,calcualtedDiscountPrice

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+function calculateTax (amount){
+    return amount*.1
+}
 
+function convertToUpperCase (text){
+    return text.toUpperCase()
+}
 
+function findMaximum (num1,num2){
+    if (num1 > num2){
+        return num1
+    }else if (num2 > num1){
+        return num2
+    }else return num1
+}
+
+function isPalindrome(word){
+    let letterArray = word.split("")
+    let reverseArray = letterArray.reverse()
+    return word === reverseArray.join("")
+}
+
+function calculateDiscountedPrice (origonalPrice, DiscountPercentage){
+    return origonalPrice*(100-DiscountPercentage)/100
+}
 
 
 
